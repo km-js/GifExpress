69c412ae343a20262c43fab2195119d67b91f9d1
Kopal Mehta <kopal.mehta22@gmail.com>
2024-08-13 18:14:57 +0530

    added routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/app-layout";
+import "./App.css";
+import Home from "./pages/home";
+import Search from "./pages/search";
+import Category from "./pages/category";
+import SingleGif from "./pages/single-gif";
+import Favorites from "./pages/favorites";
+
+//homepage
+//categories
+//search
+//single gif
+//favorites
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:category",
+        element: <Category />,
+      },
+      {
+        path: "/search/:query",
+        element: <Search />,
+      },
+      {
+        path: "/gif/:type/:slug",
+        element: <SingleGif />,
+      },
+      {
+        path: "/favorites",
+        element: <Favorites />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-  <>
-  <h1>GifExpress</h1>
-  </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
