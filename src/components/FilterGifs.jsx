fb4deade2285b40e995f1fcc3ef1469d0fa9938a
Kopal Mehta <kopal.mehta22@gmail.com>
2024-09-19 22:07:50 +0530

    created filter gifs toggle

---
+++ src/components/FilterGifs.jsx
@@ -0,0 +1,45 @@
+import { HiMiniArrowTrendingUp } from "react-icons/hi2";
+import { GifState } from "../context/gif-context";
+
+const filters = [
+  {
+    title: "GIFs",
+    value: "gifs",
+    background: "bg-gradient-to-tr from-purple-500 via-purple-600 to-purple-500",
+  },
+  {
+    title: "Stickers",
+    value: "stickers",
+    background: "bg-gradient-to-tr from-teal-500 via-teal-600 to-teal-500",
+  },
+  {
+    title: "Text",
+    value: "text",
+    background: "bg-gradient-to-tr from-blue-500 via-blue-600 to-blue-500",
+  },
+];
+
+const FilterGifs = ({ alignLeft = false, showTrending = false }) => {
+  const { filter, setFilter } = GifState();
+
+  return (
+    <div className={`flex gap-3 my-3 ${alignLeft ? "": "justify-end"} ${showTrending ? "justify-between sm:items-center flex-col sm:flex-row": ""}`}>
+      {showTrending && (
+        <span className="flex gap-2">
+        <HiMiniArrowTrendingUp size={25} 
+        className="text-teal-400" />
+        <span className="font-semibold text-grey-400">Trending</span>
+    </span>
+      )}
+
+      {/* Filters */}
+          <div className="flex min-w-80 rounded-full bg-gray-800">
+            {filters.map((f) => {
+                return <span onClick={()=> setFilter(f.value)} className={`${filter === f.value ? f.background: ""} font-semibold py-2 w-1/3 text-center rounded-full cursor-pointer`} key={f.title}>{f.title}</span>
+            })}
+          </div>
+    </div>
+  );
+};
+
+export default FilterGifs;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { GifState } from "../context/gif-context";
 import Gif from "../components/Gif";
+import FilterGifs from "../components/FilterGifs";
 
 const Home = () => {
   const { gf, gifs, setGifs, filter } = GifState();
@@ -27,7 +28,7 @@ const Home = () => {
         className="mt-2 rounded w-full"
       /> */}
 
-      {/* <FilterGif /> */}
+      <FilterGifs showTrending />
 
       <div className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2">
         {gifs.map((gif) => {
