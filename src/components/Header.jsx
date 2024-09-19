07605c0d8b9311341ced369670878a346e3a34fd
Kopal Mehta <kopal.mehta22@gmail.com>
2024-09-19 23:38:51 +0530

    implemented search bar and search page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 import { HiEllipsisVertical, HiMiniBars3BottomRight } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 import { GifState } from "../context/gif-context";
+import GifSearch from "./GifSearch";
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
   const [showCategories, setShowCategories] = useState(false);
 
-  const { gf, filter, setFilter, favorites } = GifState();
+  const { gf, favorites } = GifState();
 
   // Fetching from Giphy API
   const fetchGifCategories = async () => {
@@ -35,7 +36,11 @@ const Header = () => {
           {/* Render Categories */}
           {categories?.slice(0, 5)?.map((category) => {
             return (
-              <Link key={category.name} to={`/${category.name_encoded}`} className="px-4 py-1 hover:gradient border-b-4 hidden lg:block">
+              <Link
+                key={category.name}
+                to={`/${category.name_encoded}`}
+                className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
+              >
                 {category.name}
               </Link>
             );
@@ -50,9 +55,11 @@ const Header = () => {
             />
           </button>
 
-        {favorites.length>0 &&  <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
-            <Link to="/favorites">Favorite GIFs</Link>
-          </div>}
+          {favorites.length > 0 && (
+            <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
+              <Link to="/favorites">Favorite GIFs</Link>
+            </div>
+          )}
 
           <button>
             <HiMiniBars3BottomRight
@@ -66,16 +73,25 @@ const Header = () => {
           <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20">
             <span className="text-3xl font-extrabold">Categories</span>
             <hr className="bg-gray-100 opacity-50 my-5" />
-              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
-            {categories?.map((category)=> {
-              return <Link className="font-bold" key={category.name} to={`/${category.name_encoded}`}>{category.name}</Link>
-            })}
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+              {categories?.map((category) => {
+                return (
+                  <Link
+                    className="font-bold"
+                    key={category.name}
+                    to={`/${category.name_encoded}`}
+                  >
+                    {category.name}
+                  </Link>
+                );
+              })}
             </div>
           </div>
         )}
       </div>
 
       {/* search */}
+      <GifSearch />
     </nav>
   );
 };
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GifState } from "../context/gif-context";
+import FilterGifs from "../components/FilterGifs";
+import Gif from "../components/Gif";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [searchResults, setSearchResults] = useState([]);
+
+  const { gf, filter } = GifState();
+
+  const { query } = useParams();
+
+  const fetchSearchResults = async () => {
+    const { data } = await gf.search(query, {
+      sort: "relevant",
+      lang: "en",
+      type: filter,
+      limit: 20,
+    });
+    setSearchResults(data);
+  };
+
+  useEffect(() => {
+    fetchSearchResults();
+  }, [filter]);
+
+  return (
+    <div className="my-4">
+      <h2 className="text-5xl pb-3 font-extrabold">{query}</h2>
+
+      {/* Filter Gifs toggle bar */}
+      <FilterGifs alignLeft={true} />
+
+      {searchResults.length > 0 ? (
+        <div className="columns-2 md:columns-3 lg:columns-4 gap-2">
+          {searchResults.map((gif) => (
+            <Gif gif={gif} key={gif.id} />
+          ))}
+        </div>
+      ) : (
+        <span>
+          No Gifs found for a {query}. Try searching for Stickers instead?
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default Search;
